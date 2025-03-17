009ba5a23f07142f14e9a37c1dbf4535c97e477a
lunarifish <messenger@lunariio.work>
2025-03-17 14:13:53 +0800

    feat: pid +last_error()

--- src/librm/modules/algorithm/pid.hpp
+++ src/librm/modules/algorithm/pid.hpp
@@ -71,6 +71,7 @@ class PID {
     std::memset(error_, 0, sizeof(error_));
   }
   [[nodiscard]] f32 value() const { return utils::absConstrain(p_out_ + i_out_ + d_out_, max_out_); }
+  [[nodiscard]] f32 last_error() const { return error_[0]; }
   [[nodiscard]] f32 p_out() const { return p_out_; }
   [[nodiscard]] f32 i_out() const { return i_out_; }
   [[nodiscard]] f32 d_out() const { return d_out_; }
