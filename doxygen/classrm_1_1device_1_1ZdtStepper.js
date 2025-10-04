df5ea5e4b5b173b459f8e7b98abc09b32f3035c8
lunarifish <hnliu_2@stu.xidian.edu.cn>
2025-10-04 12:47:29 +0000

    Deploying to gh-pages from @ XDU-IRobot/librm@ecc826c62a8befc9361bdf0bc5da51bcc920bca7 🚀

--- doxygen/classrm_1_1device_1_1ZdtStepper.js
+++ doxygen/classrm_1_1device_1_1ZdtStepper.js
@@ -3,11 +3,11 @@ var classrm_1_1device_1_1ZdtStepper =
     [ "ZdtStepper", "classrm_1_1device_1_1ZdtStepper.html#abbf16e8121dd4e9735add43a1a428ec6", null ],
     [ "~ZdtStepper", "classrm_1_1device_1_1ZdtStepper.html#a1c7b26483b80aea71bcd58bd5465bffb", null ],
     [ "feedback", "classrm_1_1device_1_1ZdtStepper.html#a769f1a0e22c9aff167e12358033e3b7d", null ],
-    [ "MotorPosCtrl", "classrm_1_1device_1_1ZdtStepper.html#ab232a67b96e5e64f22f98c7f5cd018d8", null ],
-    [ "MotorSyncCtrl", "classrm_1_1device_1_1ZdtStepper.html#abc30c83b1e51e507cb26535a30a7558b", null ],
-    [ "MotorVelCtrl", "classrm_1_1device_1_1ZdtStepper.html#a785f0cb3dddbd2244b40f58bc2cb9323", null ],
-    [ "ReadPos", "classrm_1_1device_1_1ZdtStepper.html#a6546c76676fbc1fbf41af4ac97cb547f", null ],
-    [ "ReadVel", "classrm_1_1device_1_1ZdtStepper.html#a08513f2f9ca9a44d45f23852acdc1d5a", null ],
+    [ "MotorPosCtrl", "classrm_1_1device_1_1ZdtStepper.html#a282040794aa8fbfe4d4560d47642d31b", null ],
+    [ "MotorSyncCtrl", "classrm_1_1device_1_1ZdtStepper.html#a116d0ed8b48f5f586098cab7962c93f9", null ],
+    [ "MotorVelCtrl", "classrm_1_1device_1_1ZdtStepper.html#af784c3c869e75d6336cda2f83afaad29", null ],
+    [ "ReadPos", "classrm_1_1device_1_1ZdtStepper.html#af86d2b8c4b94920bd0929655c8357a91", null ],
+    [ "ReadVel", "classrm_1_1device_1_1ZdtStepper.html#a3fd984c540795a4976eb96c2bef527c1", null ],
     [ "pos", "classrm_1_1device_1_1ZdtStepper.html#afda941f79b5e972550fe55217ed24e87", null ],
     [ "vel", "classrm_1_1device_1_1ZdtStepper.html#a2b0c49a17c047ed0508324a77bf4bbdc", null ]
 ];
