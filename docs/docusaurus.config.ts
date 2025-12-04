9263e76e0a798b0f3697ef79b1ad7ab239d5de89
lunarifish <messenger@lunariio.work>
2025-12-04 15:13:34 +0800

    doc: 完善文档

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,6 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/XDU-IRobot/librm/tree/main/docs/",
+          routeBasePath: "/",
         },
         // blog: {
         //   showReadingTime: true,
