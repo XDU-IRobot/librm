1726dae9f456d043fa97c38210189ad27b4497fe
cyj <3452479291@qq.com>
2026-03-31 15:56:34 +0800

    fix: 暂时只实现了基于arm_math.h的卡尔曼滤波，后续linux可使用eigen实现

--- src/librm/modules/ahrs/kalman_filter.hpp
+++ src/librm/modules/ahrs/kalman_filter.hpp
@@ -16,7 +16,28 @@
 
 #ifdef __cplusplus
 
+#if defined(__arm__) || defined(__aarch64__) || defined(STM32)
 #include "arm_math.h"
+#else
+#include <cstdint>
+typedef struct {
+  uint16_t numRows;
+  uint16_t numCols;
+  float *pData;
+} arm_matrix_instance_f32;
+
+inline void arm_mat_init_f32(arm_matrix_instance_f32 *S, uint16_t nRows, uint16_t nCols, float *pData) {
+    S->numRows = nRows;
+    S->numCols = nCols;
+    S->pData = pData;
+}
+inline int arm_mat_add_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB, arm_matrix_instance_f32 *pDst) { return 0; }
+inline int arm_mat_sub_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB, arm_matrix_instance_f32 *pDst) { return 0; }
+inline int arm_mat_mult_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB, arm_matrix_instance_f32 *pDst) { return 0; }
+inline int arm_mat_trans_f32(const arm_matrix_instance_f32 *pSrc, arm_matrix_instance_f32 *pDst) { return 0; }
+inline int arm_mat_inverse_f32(const arm_matrix_instance_f32 *pSrc, arm_matrix_instance_f32 *pDst) { return 0; }
+#endif
+
 #include <cmath>
 #include <cstring>
 #include <cstdlib>
