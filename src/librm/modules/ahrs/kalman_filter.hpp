5fa12f10810170839f56ba2bd6a51b437e68d768
SF4cl <147938347+SF4cl@users.noreply.github.com>
2026-03-31 07:56:58 +0000

    Fix code style issues with clang-format

--- src/librm/modules/ahrs/kalman_filter.hpp
+++ src/librm/modules/ahrs/kalman_filter.hpp
@@ -27,13 +27,22 @@ typedef struct {
 } arm_matrix_instance_f32;
 
 inline void arm_mat_init_f32(arm_matrix_instance_f32 *S, uint16_t nRows, uint16_t nCols, float *pData) {
-    S->numRows = nRows;
-    S->numCols = nCols;
-    S->pData = pData;
+  S->numRows = nRows;
+  S->numCols = nCols;
+  S->pData = pData;
 }
-inline int arm_mat_add_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB, arm_matrix_instance_f32 *pDst) { return 0; }
-inline int arm_mat_sub_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB, arm_matrix_instance_f32 *pDst) { return 0; }
-inline int arm_mat_mult_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB, arm_matrix_instance_f32 *pDst) { return 0; }
+inline int arm_mat_add_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB,
+                           arm_matrix_instance_f32 *pDst) {
+  return 0;
+}
+inline int arm_mat_sub_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB,
+                           arm_matrix_instance_f32 *pDst) {
+  return 0;
+}
+inline int arm_mat_mult_f32(const arm_matrix_instance_f32 *pSrcA, const arm_matrix_instance_f32 *pSrcB,
+                            arm_matrix_instance_f32 *pDst) {
+  return 0;
+}
 inline int arm_mat_trans_f32(const arm_matrix_instance_f32 *pSrc, arm_matrix_instance_f32 *pDst) { return 0; }
 inline int arm_mat_inverse_f32(const arm_matrix_instance_f32 *pSrc, arm_matrix_instance_f32 *pDst) { return 0; }
 #endif
