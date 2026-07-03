adfebca8156927f2860740c59e802db27f88ccfb
Haonan Liu <messenger@lunariio.work>
2026-07-03 12:17:06 +0800

    fix: 7e9db88ea68afe4d3abe15b5db140f50881ec4d3 修漏了

--- src/librm/modules/angle.hpp
+++ src/librm/modules/angle.hpp
@@ -59,10 +59,10 @@ class Angle {
 namespace angle_literals {
 
 constexpr Angle operator""_deg(long double deg) {
-  return Angle{static_cast<f32>(deg * std::numbers::pi_v<float> / 180.f)};
+  return Angle{static_cast<f32>(deg * static_cast<float>(M_PI) / 180.f)};
 }
 
-constexpr Angle operator""_deg(unsigned long long deg) { return Angle{deg * std::numbers::pi_v<float> / 180.f}; }
+constexpr Angle operator""_deg(unsigned long long deg) { return Angle{deg * static_cast<float>(M_PI) / 180.f}; }
 
 constexpr Angle operator""_rad(long double rad) { return Angle{static_cast<f32>(rad)}; }
 
